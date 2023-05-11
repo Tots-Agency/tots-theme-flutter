30bcd600a08b232e7faaaa0ce660c410952a2540
Matias Camiletti <matias.camiletti@gmail.com>
2023-05-11 15:34:54 -0300

    Add fonts methods.

--- README.md
+++ README.md
@@ -5,16 +5,3 @@ Libreria para administrar el diseño de toda la aplicacion, para mantener una li
 ## Deploy library
 flutter packages pub publish --dry-run
 flutter packages pub publish
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/src/tots_theme.singleton.dart
+++ lib/src/tots_theme.singleton.dart
@@ -16,8 +16,26 @@ class TotsTheme {
   Color primaryColor = const Color.fromRGBO(60, 129, 245, 1);
   Color secondaryColor = const Color.fromRGBO(57, 210, 192, 1);
 
+  Color primaryBackground = Colors.white;
+  Color secondaryBackground = const Color.fromRGBO(249, 251, 252, 1);
+
+  Color primaryText = const Color.fromRGBO(23, 26, 43, 1);
+  Color secondaryText = const Color.fromRGBO(105, 110, 124, 1);
+
   // Métodos y propiedades de la clase Singleton
-  void setPrimaryColor(Color color) {
-    primaryColor = color;
-  }
+  TextStyle displayLarge() { return TextStyle( fontSize: 57, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle displayMedium() { return TextStyle( fontSize: 45, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle displaySmall() { return TextStyle( fontSize: 40, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle headlineLarge() { return TextStyle( fontSize: 32, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle headlineMedium() { return TextStyle( fontSize: 30, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle headlineSmall() { return TextStyle( fontSize: 24, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle titleLarge() { return TextStyle( fontSize: 22, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle titleMedium() { return TextStyle( fontSize: 18, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle titleSmall() { return TextStyle( fontSize: 16, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle labelLarge() { return TextStyle( fontSize: 14, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle labelMedium() { return TextStyle( fontSize: 12, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle labelSmall() { return TextStyle( fontSize: 11, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle bodyLarge() { return TextStyle( fontSize: 16, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle bodyMedium() { return TextStyle( fontSize: 14, fontWeight: FontWeight.normal, color: primaryText ); }
+  TextStyle bodySmall() { return TextStyle( fontSize: 12, fontWeight: FontWeight.normal, color: primaryText ); }
 }
